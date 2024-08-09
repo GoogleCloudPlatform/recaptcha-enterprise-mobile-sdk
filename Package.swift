3bc2f459e5db1e9e2578a35006dacee7152d1285
Walter Pinto <walterjp@google.com>
2024-08-09 17:26:49 -0400

    Update Package.swift with new version

--- Package.swift
+++ Package.swift
@@ -46,6 +46,7 @@ let package = Package(
     ),
     .binaryTarget(
       name: "RecaptchaEnterprise",
+      url:
         "https://dl.google.com/recaptchaenterprise/v18.6.0-beta01/RecaptchaEnterprise_iOS_xcframework/recaptcha-xcframework.xcframework.zip",
       checksum: "a088853a2efb3be8937644ea17d399ac9ef5e3f64ae2f9ef6592256f9559e374"
     ),
