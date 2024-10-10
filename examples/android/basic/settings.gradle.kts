791c5b196b07edc32941d467012e83dbba904982
Mark Corner <mcorner@gmail.com>
2024-10-10 17:24:00 -0400

    Update to 18.6.1 and switch to fetchClient API for Android

--- examples/android/basic/settings.gradle.kts
+++ examples/android/basic/settings.gradle.kts
@@ -8,6 +8,12 @@ pluginManagement {
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
+/* Used for internal testing of SDK releases
+    maven {
+      url = uri("http://localhost:1480")
+      isAllowInsecureProtocol = true
+    }
+ */
     google()
     mavenCentral()
   }
