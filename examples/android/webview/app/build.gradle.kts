46faab3af93a4fa1078dbe59b157bc131c497b93
Mark Corner <mcorner@gmail.com>
2025-02-07 11:34:45 -0500

    upgrade rce and agp

--- examples/android/webview/app/build.gradle.kts
+++ examples/android/webview/app/build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("com.google.android.material:material:1.9.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-  implementation("com.google.android.recaptcha:recaptcha:18.6.1")
+  implementation("com.google.android.recaptcha:recaptcha:18.7.0")
   implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
   implementation("androidx.activity:activity-ktx:1.7.2")
   implementation("androidx.fragment:fragment-ktx:1.6.1")
