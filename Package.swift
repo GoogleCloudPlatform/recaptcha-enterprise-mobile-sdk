4619f94ac5bc8f5c5015fef87362f9370cb5c588
Walter Pinto <walterjp@google.com>
2025-07-02 16:22:10 -0400

    Fix minimum iOS version in podspec and Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ import PackageDescription
 let package = Package(
   name: "RecaptchaEnterprise",
   platforms: [
-    .iOS(.v12)
+    .iOS(.v15)
   ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
