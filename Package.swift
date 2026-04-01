daeb68224bff52e3f26b4599a99ebaebab410bcd
walterjp <walterjp@google.com>
2026-04-01 10:32:52 -0400

    Fixing the package manager

--- Package.swift
+++ Package.swift
@@ -21,7 +21,6 @@ let package = Package(
   name: "RecaptchaEnterprise",
   platforms: [
     .iOS(.v15),
-    .visionOS(.v1)
   ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
